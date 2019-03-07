c69647cf09ca1991458422d3e10e4950440db821
Lars BOLLMANN <lars.bollmann@ist.ac.at>
2019-03-07 15:31:34 +0100

    added import

--- main.py
+++ main.py
@@ -5,7 +5,9 @@
 #
 #   Author: Lars Bollmann
 #
-#   
+#   Created: 07/03/2019
+#
+#   Structure:
 #
 #
 #
@@ -14,3 +16,25 @@
 #
 #
 ########################################################################################################################
+
+import numpy as np
+
+
+if __name__ == '__main__':
+
+    # select experiment
+    s_exp = "mjc189-1905-0517"
+
+    # load data
+    # to get rate map:
+    # data["binmat"][SESSION][CELL][TIME INTERVAL]
+    # TIME INTERVAL from data["tist"]
+    data = np.load("../02 Data/"+s_exp+"/data.npy").item()
+    x = data['binmat'][2][3][853:1514]
+    print(x)
+
+
+
+
+
+
